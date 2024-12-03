e0941ab8db4942c87432a994729507a22e0c2158
hash1ro52 <vragalev07@gmail.com>
2024-12-03 10:26:45 +0200

    :task2.2

--- src/main/java/org/collage/practice2/builder2/BankBuilder.java
+++ src/main/java/org/collage/practice2/builder2/BankBuilder.java
@@ -3,9 +3,8 @@ package org.collage.practice2.builder2;
 public class BankBuilder {
     private String name;
 
-    public BankBuilder setName(String name) {
+    public void setName(String name) {
         this.name = name;
-        return this;
     }
 
     public Bank build() {
